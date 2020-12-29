3f097bd7bb9d99ce6956d26fe6f2c3e852963249
David Yu <davidpyu@gmail.com>
2020-12-29 01:25:16 -0600

    Display initial browser-size values

--- public/js/browser-size.js
+++ public/js/browser-size.js
@@ -1,4 +1,8 @@
 function initSizeValues() {
-    console.log("hi");
     document.querySelector("#rowClientWidth > span:nth-child(2)").innerHTML = document.documentElement.clientWidth;
+    document.querySelector("#rowClientHeight > span:nth-child(2)").innerHTML = document.documentElement.clientHeight;
+    document.querySelector("#rowBodyClientWidth > span:nth-child(2)").innerHTML = document.body.clientWidth;
+    document.querySelector("#rowBodyClientHeight > span:nth-child(2)").innerHTML = document.body.clientHeight;
+    document.querySelector("#rowInnerWidth > span:nth-child(2)").innerHTML = window.innerWidth;
+    document.querySelector("#rowInnerHeight > span:nth-child(2)").innerHTML = window.innerHeight;
 }
