a8f9e892629acb083b45adaf782d38fb3b0ab76a
David Yu <davidpyu@gmail.com>
2020-12-30 13:28:40 -0600

    Added clientHeight/clientWidth and offsetWidth/offsetHeight values for 100vw/vh

--- public/js/browser-size.js
+++ public/js/browser-size.js
@@ -5,4 +5,10 @@ function initSizeValues() {
     document.querySelector("#rowBodyClientHeight > span:nth-child(2)").innerHTML = document.body.clientHeight;
     document.querySelector("#rowInnerWidth > span:nth-child(2)").innerHTML = window.innerWidth;
     document.querySelector("#rowInnerHeight > span:nth-child(2)").innerHTML = window.innerHeight;
+
+    const viewport_test = document.querySelector("#viewport_test");
+    document.querySelector("#row100VWClientWidth > span:nth-child(2)").innerHTML = viewport_test.clientWidth;
+    document.querySelector("#row100VHClientHeight > span:nth-child(2)").innerHTML = viewport_test.clientHeight; 
+    document.querySelector("#row100VWOffsetWidth > span:nth-child(2)").innerHTML = viewport_test.offsetWidth;
+    document.querySelector("#row100VHOffsetHeight > span:nth-child(2)").innerHTML = viewport_test.offsetHeight;
 }
